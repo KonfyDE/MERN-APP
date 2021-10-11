d0e50b9ed6c6210aa787593cf771f4f6c908efc3
kolofotias <vkolofotias@gmail.com>
2021-10-11 20:44:26 +0200

    Start

--- client/src/App.js
+++ client/src/App.js
@@ -24,10 +24,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Mern-app</h1>
+        <h1>KONFY MVP</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
